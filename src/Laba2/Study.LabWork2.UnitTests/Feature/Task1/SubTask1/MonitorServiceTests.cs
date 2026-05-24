ff85b99ade3cea566c1b31a7d9206b9739573dd2
hukus <lnsnika@yandex.ru>
2026-05-25 01:43:57 +0300

    Лаборатрная 2 задание 1.1

--- src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MonitorServiceTests.cs
+++ src/Laba2/Study.LabWork2.UnitTests/Feature/Task1/SubTask1/MonitorServiceTests.cs
@@ -1,6 +1,36 @@
+using NUnit.Framework;
+using Study.LabWork2.Feature.Task1.SubTask1;
+
 namespace Study.LabWork2.UnitTests.Feature.Task1.SubTask1;
 
 [TestFixture]
 public sealed class MonitorServiceTests
 {
+    private MonitorService _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _service = new MonitorService();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var result = _service.CountPrimes(1, 100, 2);
+        Assert.That(result.PrimeCount, Is.EqualTo(25));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var result = _service.CountPrimes(1, 10000, 4);
+        Assert.That(result.PrimeCount, Is.EqualTo(1229));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.That(_service.GetVersionName(), Is.EqualTo("Monitor"));
+    }
 }
